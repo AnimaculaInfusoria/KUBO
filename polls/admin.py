abd5ff215860279f1d97ea391d2b185c68f3a507
Kubo Lehotsky <kubolehotsky@gmail.com>
2022-01-19 09:13:48 +0100

    old db removed

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from .models import Question, Choice
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
+from .models import Question, Choice
+# Register your models here.
 class Choiceline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -20,13 +22,17 @@ class QuestionAdmin(admin.ModelAdmin):
 admin.site.register(Question, QuestionAdmin)
 
 
+
+# WAGTAIL ADMIN SECTION
 class PollsAdmin(ModelAdmin):
     model = Question
     menu_label = "Polls"
     menu_icon = "plus"
-    inlines = [Choiceline]
-    list_display = ('question_text', 'pub_date')
-    search_fields = ['question_text']
+    menu_order = 300
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("question_text", "pub_date",)
+    list_filter = ('question_text',)
+    search_fields = ("question_text",)
 
 modeladmin_register(PollsAdmin)
-
