db27170076dbade3f2ac3cc712554ef31743a9c0
Kubo Lehotsky <kubolehotsky@gmail.com>
2022-01-20 21:25:40 +0100

    blocks

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel 
 from django.db import models
-
+from wagtail.core import blocks
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from polls.models import Question
+from polls import blocks_polls
 
 
 class BlogListingPage(Page):
@@ -28,15 +32,23 @@ class BlogListingPage(Page):
         ]
 
 class BlogPage(Page):
-    date = models.DateField("Article Date")
-    intro = models.TextField("Introduction")
-    body = RichTextField(
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
         blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
     )
+    poll = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True, related_name='+')
+    text = StreamField([
+        ('text_fill', blocks.RichTextBlock()),
+        ('polls', blocks_polls.PollsBlock()),
+    ], default=None)
 
+    # Editor panels configuration
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
+        ImageChooserPanel('main_image'),
+        StreamFieldPanel('text'),
+        FieldPanel('owner'),
     ]
---
+++ polls/blocks_polls.py
@@ -0,0 +1,11 @@
+from wagtail.core import blocks
+from wagtail.snippets.blocks import SnippetChooserBlock
+from .models import Question
+
+class PollsBlock(blocks.StructBlock):
+    # v blocku si vyberame s questions...pozivame SnippetChooserBlock (nie Panel)
+    poll = SnippetChooserBlock(Question)
+    class Meta:
+        template = 'polls/snippets/poll_snippet.html'
+        icon = 'edit'
+        label = 'Poll'
